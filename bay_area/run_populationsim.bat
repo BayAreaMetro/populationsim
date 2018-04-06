b731ebc5bb273c350a74a1e9355f4f8d3e26b3b2
Lisa Zorn <lmz@alumni.brown.edu>
2018-04-06 12:59:55 -0700

    Fix batch syntax error

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -2,7 +2,7 @@
 
 :create_seed
 python create_seed_population.py
-if ERRORLEVEL 1 then goto error
+if ERRORLEVEL 1 goto error
 
 :year_loop
 for %%Y in (2000 2005 2010 2015) do (
@@ -10,12 +10,12 @@ for %%Y in (2000 2005 2010 2015) do (
   rem households
   mkdir households\output_%%Y
   python run_populationsim.py --model_year %%Y  --config households\configs     --output households\output_%%Y    --data households\data
-  if ERRORLEVEL 1 then goto error
+  if ERRORLEVEL 1 goto error
 
   rem group_quarters
   mkdir group_quarters\output_%%Y
   python run_populationsim.py --model_year %%Y --config group_quarters\configs --output group_quarters\output_%%Y  --data group_quarters\data
-  if ERRORLEVEL 1 then goto error
+  if ERRORLEVEL 1 goto error
 )
 
 :success
