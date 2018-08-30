9a5b7868301f8c48b193bfacb531c5cade149b0f
Lisa Zorn <lzorn@bayareametro.gov>
2018-08-29 21:25:24 -0700

    Try adding importance on difficult controls
    And update final script to add county occupation household summaries

--- bay_area/combine_households_gq.py
+++ bay_area/combine_households_gq.py
@@ -7,6 +7,7 @@ USAGE = r"""
   (2) [households,group_quarters]/output_[model_year]/synthetic_persons.csv
   (3) [households,group_quarters]/output_[model_year]/summary_melt.csv
   (4) group_quarters/data/geo_cross_walk.csv
+  (5) households/data/[model_year]_COUNTY_controls.csv
 
   Outputs:
   (1) output_[model_year]/synthetic_households.csv
@@ -17,9 +18,10 @@ USAGE = r"""
   (a) Concatenates person and housing records for households and group quarters, creating a
       unique HHID and PERID.
   (b) Add TAZ to group quarters from MAZ (it's not there since there are no TAZ level controls)
-  (c) Add MTCCountyID to all 
+  (c) Add MTCCountyID to all
   (c) Fills NaN values with -9
   (d) Downcasts columns to int
+  (e) Adds county-level household summaries to summary_melt since they're not there.
 
 """
 
@@ -77,6 +79,8 @@ if __name__ == '__main__':
   geocrosswalk_df = pandas.read_csv(os.path.join("group_quarters","data","geo_cross_walk.csv"))
   print(geocrosswalk_df.head())
 
+  hh_counties_df = None
+
   for filename in ["synthetic_households.csv", "synthetic_persons.csv", "summary_melt.csv"]:
     table_hh = pandas.read_csv(os.path.join("households",     COMBINED_OUT_DIR, filename))
     table_gq = pandas.read_csv(os.path.join("group_quarters", COMBINED_OUT_DIR, filename))
@@ -97,6 +101,9 @@ if __name__ == '__main__':
       # the households doesn't have county so get county from crosswalk
       table_hh = pandas.merge(left=table_hh, right=geocrosswalk_df[["MAZ","COUNTY"]], how="left")
 
+      # save for creating county summaries
+      hh_counties_df = table_hh[["HHID","COUNTY"]]
+
     elif filename=="synthetic_persons.csv":
       # add HHID
       table_hh["HHID"] = table_hh.unique_hh_id
@@ -106,10 +113,43 @@ if __name__ == '__main__':
       print "Max housing record PERID: %d" % max_perid
       table_gq["PERID"] = table_gq.index + 1 + max_perid # start from max_perid + 1
 
+      # create county summaries - only want employed (see controls.csv)
+      hh_pers_county_occ_summary = pandas.merge(left  =table_hh.loc[ table_hh["employed"] == 1, ["HHID","PERID","occupation"]],
+                                                right =hh_counties_df,
+                                                how   ="left",
+                                                on    ="HHID")
+      hh_pers_county_occ_summary = hh_pers_county_occ_summary.groupby(["COUNTY","occupation"]).agg("count").reset_index()
+
+      hh_pers_county_occ_summary.rename(columns={"COUNTY":"id", "occupation":"variable", "PERID":"result"}, inplace=True)
+      hh_pers_county_occ_summary.drop(columns="HHID",inplace=True)
+      hh_pers_county_occ_summary["geography"] = "COUNTY"
+      hh_pers_county_occ_summary["type"]      = "households"
+      hh_pers_county_occ_summary.replace(to_replace={"variable":  # recode variable
+                                         {1:"pers_occ_management",
+                                          2:"pers_occ_professional",
+                                          3:"pers_occ_services",
+                                          4:"pers_occ_retail",
+                                          5:"pers_occ_manual",
+                                          6:"pers_occ_military"}}, inplace=True)
+      print(hh_pers_county_occ_summary.head())
+
     elif filename=="summary_melt.csv":
       # just add a column for type
       table_hh["type"] = "households"
       table_gq["type"] = "group_quarters"
+      # add county summaries
+      county_hh_controls_df = pandas.read_csv(os.path.join("households","data","{}_COUNTY_controls.csv".format(args.model_year)))
+      county_hh_controls_df = county_hh_controls_df[["COUNTY","pers_occ_management",
+        "pers_occ_professional","pers_occ_services","pers_occ_retail","pers_occ_manual","pers_occ_military"]]
+      county_hh_controls_df = county_hh_controls_df.melt(id_vars=["COUNTY"], var_name="variable", value_name="control")
+      county_hh_controls_df.rename(columns={"COUNTY":"id"}, inplace=True)
+      print(county_hh_controls_df.head())
+      county_hh_controls_df = pandas.merge(left=county_hh_controls_df, right=hh_pers_county_occ_summary, how="left")
+      county_hh_controls_df["diff"] = county_hh_controls_df["result"] - county_hh_controls_df["control"]
+      print(county_hh_controls_df.head())
+
+      county_hh_controls_df = county_hh_controls_df[ list(table_hh.columns) ]  # reorder columns to match
+      table_hh = pandas.concat([table_hh, county_hh_controls_df])
 
     concat_table = pandas.concat([table_hh, table_gq])
     concat_table.fillna(value=-9, inplace=True)
