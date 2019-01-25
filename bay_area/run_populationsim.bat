62cb7713cdc3f17bad310aa185c7a9bc7d871afc
Lisa Zorn <lzorn@bayareametro.gov>
2019-01-25 15:38:42 -0800

    Updating batch for Clean and Green Draft 1

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,8 +13,8 @@ set MODELTYPE=TM1
 
 :: should be the urbansim run number from the control files
 set PETRALEPATH=X:\petrale
-set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Rising Tides Falling Fortunes (S2)\2018 12 21
-set BAUS_RUNNUM=run16
+set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Clean and Green (S1)\2019 01 23
+set BAUS_RUNNUM=run20
 
 :: assume argument is year
 set YEARS=%1
