8446710b746541311e37c5d8f3139086ec7ad4c2
MTCPB <MTCPB@mtc.ca.gov>
2019-02-15 20:52:06 -0800

    Clean and Green - 2019 02 15 - run60

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,9 +13,9 @@ set MODELTYPE=TM1
 
 :: should be the urbansim run number from the control files
 set PETRALEPATH=X:\petrale
-set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Rising Tides Falling Fortunes (S2)\2019 02 15
-set BAUS_RUNNUM=run59
-set OUTPUT_SUFFIX=RisingTides_20190215_!BAUS_RUNNUM!
+set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Clean and Green (S1)\2019 02 15 ProjectPerformance
+set BAUS_RUNNUM=run60
+set OUTPUT_SUFFIX=CleanAndGreen_20190215_!BAUS_RUNNUM!
 
 :: assume argument is year
 set YEARS=%1
