f2feeafee1f9cfba026245ff63f4a6210687106a
Lisa Zorn <lzorn@bayareametro.gov>
2019-08-29 18:20:44 -0700

    RisingRTides, 2019 08 28 Round2 v0, run36

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,9 +13,9 @@ set MODELTYPE=TM1
 
 :: should be the urbansim run number from the control files
 set PETRALEPATH=X:\petrale
-set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Rising Tides Falling Fortunes (S2)\2019 08 14 FR1 re-run v1
-set BAUS_RUNNUM=run26
-set OUTPUT_SUFFIX=RisingTides_20190814_!BAUS_RUNNUM!
+set URBANSIMPATH=\\tsclient\C\Users\lzorn\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Rising Tides Falling Fortunes (S2)\2019 08 28 Futures Round 2 v0
+set BAUS_RUNNUM=run36
+set OUTPUT_SUFFIX=RisingTides_20190828_!BAUS_RUNNUM!
 
 :: assume argument is year
 set YEARS=%1
