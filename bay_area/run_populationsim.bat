0f4f973142c7c851f1e9186886e522b045471c58
MTCPB <MTCPB@mtc.ca.gov>
2019-09-09 18:14:46 -0700

    Back To the Future, Futures Round 2 iteration 2, run15

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,9 +13,9 @@ set MODELTYPE=TM1
 
 :: should be the urbansim run number from the control files
 set PETRALEPATH=X:\petrale
-set URBANSIMPATH=\\tsclient\C\Users\ftsang\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Clean and Green (S1)\2019 09 09 FR2 v0 iteration 2
+set URBANSIMPATH=\\tsclient\C\Users\ftsang\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\Horizon\Output\Back to the Future (S5)\2019 09 09 FR2 v0 iteration 2
 set BAUS_RUNNUM=run15
-set OUTPUT_SUFFIX=CleanAndGreen_20190909_!BAUS_RUNNUM!
+set OUTPUT_SUFFIX=BackToTheFuture_20190909_!BAUS_RUNNUM!
 
 :: assume argument is year
 set YEARS=%1
