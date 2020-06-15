4a72f882ab519e14ad503e74bdbfb365beb7920e
Flavia <flavia.tsang@gmail.com>
2020-06-15 09:06:41 -0700

    restore the configs to the configs used in Horizon

--- bay_area/add_hhgq_combined_controls.py
+++ bay_area/add_hhgq_combined_controls.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     parser.add_argument("--model_year", type=int, required=True, help="Model year")
     args = parser.parse_args()
 
-    # control files are:
+    # control files are: 
     #  - [run_num]_taz_summaries_[year].csv
     taz_controls_file = os.path.join("hh_gq", "data", "{}_TAZ_summaries_{}.csv".format(args.run_num, args.model_year))
     taz_controls_df   = pandas.read_csv(taz_controls_file)
@@ -25,31 +25,8 @@ if __name__ == '__main__':
 
     # total households: combine actual tothh + gq_tot_pop
     taz_controls_df["numhh_gq"] = taz_controls_df.TOTHH + taz_controls_df.gq_tot_pop
-    # deal with unreasonably high pop per hh (only 1 case in Draft Blueprint Plus Crossing run - TAZ1272)
-    # if the pop per hh is unreasonably high (e.g. greater than 10), take max(gq_tot_pop, GQPOP)
-    taz_controls_df["numhh_gq"] = numpy.where(taz_controls_df['TOTPOP']/taz_controls_df['numhh_gq'] > 10, taz_controls_df.TOTHH + numpy.where(taz_controls_df.gq_tot_pop >= taz_controls_df.GQPOP, taz_controls_df.gq_tot_pop, taz_controls_df.GQPOP), taz_controls_df["numhh_gq"])
-
     # GQ are 1-person households
     taz_controls_df["hh_size_1_gq"] = taz_controls_df.hh_size_1 + taz_controls_df.gq_tot_pop
-    # deal with unreasonably high pop per hh (only 1 case in Draft Blueprint Plus Crossing run - TAZ1272)
-    taz_controls_df["hh_size_1_gq"] = numpy.where(taz_controls_df['TOTPOP']/taz_controls_df['numhh_gq'] > 10, taz_controls_df.hh_size_1 + numpy.where(taz_controls_df.gq_tot_pop >= taz_controls_df.GQPOP, taz_controls_df.gq_tot_pop, taz_controls_df.GQPOP), taz_controls_df["hh_size_1_gq"])
-
-    # address households and population conflicts
-    # where households > population, let household numbers take precedence and adjust total population (assume 2 persons per hh)
-    taz_controls_df['totpop_adj']  = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], taz_controls_df['numhh_gq'] * 2, taz_controls_df['TOTPOP'])
-    taz_controls_df['AGE0004_adj'] = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], numpy.where(taz_controls_df['TOTPOP'] > 0, taz_controls_df['AGE0004'] * taz_controls_df['totpop_adj'] / taz_controls_df['TOTPOP'], taz_controls_df['totpop_adj']/5), taz_controls_df['AGE0004'])
-    taz_controls_df['AGE0519_adj'] = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], numpy.where(taz_controls_df['TOTPOP'] > 0, taz_controls_df['AGE0519'] * taz_controls_df['totpop_adj'] / taz_controls_df['TOTPOP'], taz_controls_df['totpop_adj']/5), taz_controls_df['AGE0519'])
-    taz_controls_df['AGE2044_adj'] = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], numpy.where(taz_controls_df['TOTPOP'] > 0, taz_controls_df['AGE2044'] * taz_controls_df['totpop_adj'] / taz_controls_df['TOTPOP'], taz_controls_df['totpop_adj']/5), taz_controls_df['AGE2044'])
-    taz_controls_df['AGE4564_adj'] = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], numpy.where(taz_controls_df['TOTPOP'] > 0, taz_controls_df['AGE4564'] * taz_controls_df['totpop_adj'] / taz_controls_df['TOTPOP'], taz_controls_df['totpop_adj']/5), taz_controls_df['AGE4564'])
-    taz_controls_df['AGE65P_adj']  = numpy.where(taz_controls_df['numhh_gq'] > taz_controls_df['TOTPOP'], numpy.where(taz_controls_df['TOTPOP'] > 0, taz_controls_df['AGE65P']  * taz_controls_df['totpop_adj'] / taz_controls_df['TOTPOP'], taz_controls_df['totpop_adj']/5), taz_controls_df['AGE65P'])
-
-    # If non-zero totpop but 0 household, let household numbers take precedence and set totpop to 0.
-    taz_controls_df['totpop_adj']  = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['totpop_adj'])
-    taz_controls_df['AGE0004_adj'] = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['AGE0004_adj'])
-    taz_controls_df['AGE0519_adj'] = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['AGE0519_adj'])
-    taz_controls_df['AGE2044_adj'] = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['AGE2044_adj'])
-    taz_controls_df['AGE4564_adj'] = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['AGE4564_adj'])
-    taz_controls_df['AGE65P_adj']  = numpy.where(taz_controls_df["numhh_gq"]==0, 0, taz_controls_df['AGE65P_adj'])
 
     # note that hh_wrks and hh_inc categories specify households.TYPE==1 so no need to modify those
 
