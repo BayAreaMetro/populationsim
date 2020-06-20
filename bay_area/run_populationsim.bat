e3c3c7fb4291144d10fb45f7ec0100a74ce50ea6
Flavia <flavia.tsang@gmail.com>
2020-06-20 09:31:53 -0700

    Creation of POPLU_v170_01

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,9 +13,9 @@ set MODELTYPE=TM1
 
 :: should be the urbansim run number from the control files
 set PETRALEPATH=X:\petrale
-set URBANSIMPATH=\\tsclient\C\Users\ftsang\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\PBA50\Draft Blueprint runs\Blueprint Plus Crossing (s23)\v1.6 (all strategies)
-set BAUS_RUNNUM=run90
-set OUTPUT_SUFFIX=PBA50PlusCrossing_20200619_!BAUS_RUNNUM!
+set URBANSIMPATH=\\tsclient\C\Users\ftsang\Box\Modeling and Surveys\Urban Modeling\Bay Area UrbanSim 1.5\PBA50\Draft Blueprint runs\Blueprint Plus Crossing (s23)\v1.7 (strategies + BASIS-hybrid)
+set BAUS_RUNNUM=run92
+set OUTPUT_SUFFIX=PBA50PlusCrossing_20200620_!BAUS_RUNNUM!
 
 :: assume argument is year
 set YEARS=%1
