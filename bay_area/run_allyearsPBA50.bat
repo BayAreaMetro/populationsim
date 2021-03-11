689d318a78726b657e36c79ed9733d634776dc78
Flavia <flavia.tsang@gmail.com>
2021-03-11 13:17:50 -0800

    the last commit message should say "BAUS EIR Alt2_v1" - this is a dummy commit

--- bay_area/run_allyearsPBA50.bat
+++ bay_area/run_allyearsPBA50.bat
@@ -1,6 +1,5 @@
-
 :: Batch script to run the population simulation for all years required for PBA50
-:: 2050, 2040, 2035, 2030, 2025
+:: five time points are: 2050, 2040, 2035, 2030, 2025
 
 :: prioritize 2050 and 2035
 call run_populationsim 2050
@@ -10,3 +9,4 @@ call run_populationsim 2035
 call run_populationsim 2040
 call run_populationsim 2030
 call run_populationsim 2025
+
