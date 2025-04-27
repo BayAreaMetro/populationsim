daa7928b15b85fdee848efa763abe6da592e9aae
Lisa Zorn <lzorn@bayareametro.gov>
2025-04-26 20:05:25 -0700

    Create FBP_BAUS_v8 from PBA50Plus_Final_Blueprint_v64

--- bay_area/run_populationsim.bat
+++ bay_area/run_populationsim.bat
@@ -13,11 +13,11 @@ set MODELTYPE=TM1
 :: for a forecast, copies marginals from         "%URBANSIMPATH%\travel_model_summaries\%BAUS_RUNNUM%_xxx_summaries_!YEAR!.csv"
 :: for past/current year, copies marginals from  "%TMPATH%\!YEAR!""
 set TMPATH=X:\travel-model-one-v1.6.1_develop\utilities\taz-data-baseyears
-set URBANSIMPATH=M:\urban_modeling\baus\PBA50Plus\PBA50Plus_FinalBlueprint\PBA50Plus_Final_Blueprint_v58
+set URBANSIMPATH=M:\urban_modeling\baus\PBA50Plus\PBA50Plus_FinalBlueprint\PBA50Plus_Final_Blueprint_v64
 :: used in OUTPUT_SUFFIX as well; use "census" for non-BAUS-based run
-set BAUS_RUNNUM=PBA50Plus_Final_Blueprint_v58
+set BAUS_RUNNUM=PBA50Plus_Final_Blueprint_v64
 :: OUTPUT DIR will be X:\populationsim_outputs\hh_gq\output_!OUTPUT_SUFFIX!_!YEAR!!PUMA_SUFFIX!_!BAUS_RUNNUM!
-set OUTPUT_SUFFIX=FBP_20250423
+set OUTPUT_SUFFIX=FBP_20250426
 
 :: assume argument is year
 set YEARS=%1
