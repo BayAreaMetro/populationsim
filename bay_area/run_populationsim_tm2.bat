c6a14da39c489065f4e2350c10d64eabadbf26ee
Suzanne Childress <childresssuzanne@gmail.com>
2025-07-24 10:16:46 -0700

    getting updated seed households and persons

--- bay_area/run_populationsim_tm2.bat
+++ bay_area/run_populationsim_tm2.bat
@@ -9,11 +9,11 @@ setlocal EnableDelayedExpansion
 
 :: should be TM1 or TM2
 set MODELTYPE=TM2
-set YEAR=2015
+set YEAR=2023
 
 :: for a forecast, copies marginals from         "%URBANSIMPATH%\travel_model_summaries\%BAUS_RUNNUM%_xxx_summaries_!YEAR!.csv"
 :: for past/current year, copies marginals from  "%TMPATH%\!YEAR!""
-set TMPATH=C:\GitHub\tm2popsim_in_out\%YEAR%\inputs
+set TMPATH=output_2023
 ::set URBANSIMPATH=M:\urban_modeling\baus\PBA50Plus\PBA50Plus_FinalBlueprint\PBA50Plus_Final_Blueprint_v65
 :: used in OUTPUT_SUFFIX as well; use "census" for non-BAUS-based run
 ::set BAUS_RUNNUM=BAUS_RUNNUM=PBA50Plus_Final_Blueprint_v65
@@ -21,7 +21,7 @@ set TMPATH=C:\GitHub\tm2popsim_in_out\%YEAR%\inputs
 ::set OUTPUT_SUFFIX=FBP_20250522
 
 rem create the final output directory that populationsim will write to
-set OUTPUT_DIR=C:\GitHub\tm2popsim_in_out\%YEAR%\outputs
+set OUTPUT_DIR=output_2023\populationsim_run
 echo OUTPUT_DIR=[!OUTPUT_DIR!]
 if not exist !OUTPUT_DIR! ( mkdir !OUTPUT_DIR! )
 
@@ -44,6 +44,7 @@ if "%TEST_PUMA%" NEQ "" (
 
 :create_seed
 if not exist hh_gq\data\seed_households.csv (
+  echo Creating seed population files...
   python create_seed_population.py
   if ERRORLEVEL 1 goto error
 )
@@ -95,6 +96,20 @@ for %%Y in (!YEARS!) do (
       fc /b "%TMPATH%\maz_marginals.csv"    hh_gq\data\maz_marginals.csv > nul
       if errorlevel 1 goto error
       fc /b "%TMPATH%\taz2_marginals.csv"   hh_gq\data\taz_marginals.csv > nul
+      if errorlevel 1 goto error
+      fc /b "%TMPATH%\county_marginals.csv" hh_gq\data\county_marginals.csv > nul
+      if errorlevel 1 goto error
+
+    )
+    if !YEAR!==2023 (
+      copy /y "%TMPATH%\maz_marginals.csv"    hh_gq\data\maz_marginals.csv
+      copy /y "%TMPATH%\taz_marginals.csv"   hh_gq\data\taz_marginals.csv
+      copy /y "%TMPATH%\county_marginals.csv" hh_gq\data\county_marginals.csv
+
+      rem Verify that the file copy MUST succeed or we'll run populationsim with the wrong input
+      fc /b "%TMPATH%\maz_marginals.csv"    hh_gq\data\maz_marginals.csv > nul
+      if errorlevel 1 goto error
+      fc /b "%TMPATH%\taz_marginals.csv"   hh_gq\data\taz_marginals.csv > nul
       if errorlevel 1 goto error
       fc /b "%TMPATH%\county_marginals.csv" hh_gq\data\county_marginals.csv > nul
       if errorlevel 1 goto error
